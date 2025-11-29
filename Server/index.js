b0d12ad1dc6d1a2656d227b3a4b4c4204d7014d9
vishal-dev24 <vishalmalviya3846@gmail.com>
2025-11-29 23:31:01 +0530

    update index.js

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["https://test-pinterest-1.onrender.com"],
+    origin: ["https://test-pinterest-1.onrender.com", "https://test-pinterest.onrender.com"],
     credentials: true
 }));
 
