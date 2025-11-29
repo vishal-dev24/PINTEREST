b4b0614b44d57ff17ef7d517372425ba61628f8a
vishal-dev24 <vishalmalviya3846@gmail.com>
2025-11-29 23:28:41 +0530

    update index.js

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,11 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+    origin: ["https://test-pinterest-1.onrender.com"],
+    credentials: true
+}));
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
