86baa15fcf1d99794ac9f860821cd5a952e7932a
vishal-dev24 <vishalmalviya3846@gmail.com>
2026-02-16 17:22:25 +0530

    update server

--- Server/index.js
+++ Server/index.js
@@ -12,14 +12,7 @@ app.use(cookieParser());
 
 const cors = require("cors");
 
-app.use(cors({
-    origin: "https://test-pinterest.onrender.com",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-}));
-
-app.options("*", cors());
-
+app.use(cors({ origin: ['https://test-pinterest.onrender.com', 'https://test-pinterest-1.onrender.com'], credentials: true }));
 
 const SECRET = "shhhh";
 
